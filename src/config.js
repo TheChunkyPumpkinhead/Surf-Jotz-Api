d714d09b4707227bb490173a787595d5746f2f57
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-06-09 10:46:46 -0700

    New url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://chris@localhost/jotz',
-  TEST_DATABASE_URL:process.env.DATABASE_URL || 'postgresql://chris@localhost/jotz'
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgres://dev_lmqw_user:ViHS0rsgcXH7NChIlKJ9nnDJqppN2GEi@dpg-cph964ocmk4c73ecvs2g-a.oregon-postgres.render.com/dev_lmqw",
+  TEST_DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgres://dev_lmqw_user:ViHS0rsgcXH7NChIlKJ9nnDJqppN2GEi@dpg-cph964ocmk4c73ecvs2g-a.oregon-postgres.render.com/dev_lmqw",
 };
